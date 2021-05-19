6fd114d5be897b712a4a60dd7a04ad1c65ff707d
vantwoutmaarten <m.a.vantwout@student.tudelft.nl>
2021-05-19 16:29:25 +0200

    stockprice shifted column with regular 90percent and 33 percent missingness working for AAPL, KO and MSFT

--- code/data_price/multivariate_price_data_creator.py
+++ code/data_price/multivariate_price_data_creator.py
@@ -5,10 +5,11 @@ import pandas_datareader as web
 import copy
 
 
-#copies the stock price, makes 1 new column 30 values ahead, makes 1 new column with 30% missing and one with 90% missing.
+#copies the stock price, makes 1 new column 30 values ahead, then this column is copied and made sparse in two ways.
+# one way: new copied column with 30% missing. Second way:new copied column with 90% missing.
 def create_2D_shifted_stockprice(scenario_name):
 
-    df = web.DataReader('AAPL', data_source='yahoo', start='2016-12-31',  end='2020-12-31')
+    df = web.DataReader('MSFT', data_source='yahoo', start='2016-12-31',  end='2020-12-31')
     total_days = df['Close'].count()
     data = df.filter(['Close'])
 
@@ -22,47 +23,43 @@ def create_2D_shifted_stockprice(scenario_name):
     df_lagged = pd.concat((df_lagged, shifted), axis=1)
 
     df_lagged = df_lagged.dropna()
+    print("size before missing")
+    print(df_lagged.size)
     # with this the index can be dropped, but then that should be done after later concatenations aswell, 
     # This should not be used when matching price columns with quarterly figure columns, since these should be concatinated by date.
-    # Now this is done with numerical index, because it is easier to modify the properties such as sparseness, such as using #days as index.
+    # Now this is not done to have similarity between the test data and eventual quarterly figure data. (got the missingness working with dates soNice)
     # df_lagged = df_lagged.reset_index(drop=True)
     # shifted = shifted.reset_index(drop=True)
 
-    # This creates a new column with 30 sparseness
+    # This creates a new column with 33% missingness
     close_ahead30_missing30 = df_lagged.filter(['Close_ahead30']).copy()
     close_ahead30_missing30.columns = [x + "_missing" + str(30) for x in df_lagged.filter(['Close_ahead30']).columns]
 
     day = 0
+    #Every third value is missing = 33% missingness
     for key, value in close_ahead30_missing30.iterrows():
-        print(key, value)
         if(day%3 == 2):
-            print("MAKE NP NAN VALUES")
-            close_ahead30_missing30[key] = np.nan
-        print(key, value)
+            close_ahead30_missing30.loc[key,'Close_ahead30_missing30'] = np.nan
         day = day + 1
-        print(day)
-
-    # while(day < total_days):
-    #     if(day%3 == 2):
-    #         print("this column is changed.")
-    #         print(str(close_ahead30_missing30[day]))
-    #         close_ahead30_missing30[day] = np.nan
-    #     day = day + 1
-
-    # close_ahead30_missing90 = shifted.copy()
-    # close_ahead30_missing90.columns = [x + "_missing" + str(90) for x in shifted.columns]
-
-    # day = 0
-    # while(day < total_days):
-    #     if(day%10 == 9):
-    #         close_ahead30_missing90[day] = np.nan
-    #     day = day + 1
 
     df_lagged = pd.concat((df_lagged, close_ahead30_missing30), axis=1)
-    # df_lagged = df_lagged.reset_index(drop=True)
+
+
+    # This creates a new column with 90 missingness
+    close_ahead30_missing90 = df_lagged.filter(['Close_ahead30']).copy()
+    close_ahead30_missing90.columns = [x + "_missing" + str(90) for x in df_lagged.filter(['Close_ahead30']).columns]
+
+    day = 0
+    #(Every 1st of 10 values is present, then 9/10 missing) Thus, 90% missingness
+    for key, value in close_ahead30_missing90.iterrows():
+        if(day%10 != 0):
+            close_ahead30_missing90.loc[key,'Close_ahead30_missing90'] = np.nan
+        day = day + 1
+    
+    df_lagged = pd.concat((df_lagged, close_ahead30_missing90), axis=1)
 
     output_loc = 'data_price/' + scenario_name
 
     df_lagged.to_csv(output_loc)
 
-create_2D_shifted_stockprice(scenario_name = 'AAPL_Shifted_30ahead.csv')
+create_2D_shifted_stockprice(scenario_name = 'MSFT_Shifted_30ahead.csv')
